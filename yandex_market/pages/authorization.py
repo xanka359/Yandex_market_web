c3de7185150223ea954c2f8740a06de6cbe51c8f
Oksana Bondareva <xanka359@yandex.ru>
2024-05-20 23:10:23 +0300

    fix with authorization anf req

--- yandex_market/pages/authorization.py
+++ yandex_market/pages/authorization.py
@@ -1,7 +1,8 @@
 import os
 
+import allure
 from dotenv import load_dotenv
-from selene import browser, have, by
+from selene import browser, have, by, command
 from selene.support.shared.jquery_style import s
 
 load_dotenv()
@@ -11,15 +12,18 @@ class Authorization:
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
 
+    @allure.step('Открыть главную страницу ')
     def open(self):
         browser.open('/')
         return self
 
+    @allure.step('Открыть страницу авторизации')
     def open_login_page(self):
         s('#USER_MENU_ANCHOR').element(by.text('Войти')).click()
         s('[data-type="login"]').click()
         return self
 
+    @allure.step('Заполнить логин и пароль корректными данными')
     def fill_login_form_valid_credentials(self, login, password):
         s('#passp-field-login').type(f'{login}')
         s('.passp-sign-in-button').should(have.text('Войти')).click()
@@ -27,18 +31,21 @@ class Authorization:
         s('.passp-sign-in-button').should(have.text('Продолжить')).click()
         return self
 
+    @allure.step('Проверить имя авторизованного пользователя')
     def check_authorized_info(self):
         s('#USER_MENU_ANCHOR').click()
         s('.Npykn').element('[data-auto="public-user-info"]').should(have.text('Ксения А.'))
-        browser.driver.execute_script('window.scrollBy(-50, 0);')
+        browser.element('.Npykn').perform(command.js.scroll_into_view)
         return self
 
+    @allure.step('Заполнить логин и пароль некорректными данными ')
     def fill_login_form_invalid_credentials(self):
         s('#passp-field-login').type('someLogin')
         s('.passp-sign-in-button').should(have.text('Войти')).click()
         s('#passp-field-passwd').click().type('somePassword')
         s('.passp-sign-in-button').should(have.text('Продолжить')).click()
 
+    @allure.step('Проверить ошибку авторизации')
     def check_error_for_invalid_credentials(self):
         s('.Field-inputWrapper').should(have.text('Неверный пароль'))
 
